f15fc7f6531f51cb35c1f3db96c94c7b025a2eb8
Wojtek Mielczarek, Rafal Fronczyk <wojtek.mielczarek@u2i.com, rafal.fronczyk@u2i.com>
2014-07-01 09:19:49 +0200

    Partitions parameter added to geohose method

--- twitter4j-stream/src/main/java/twitter4j/TwitterStream.java
+++ twitter4j-stream/src/main/java/twitter4j/TwitterStream.java
@@ -68,11 +68,12 @@ public interface TwitterStream extends OAuthSupport, TwitterBase {
      * Starts listening on all public statuses and includes geolocation information as value of user/filters/placeid. Available only to approved parties and requires a signed agreement to access. Please do not contact us about access to the firehose. If your service warrants access to it, we'll contact you.
      *
      * @param count Indicates the number of previous statuses to stream before transitioning to the live stream.
+     * @param partitions Lists partition numbers separated by comma as a string e.g. "0,1,2,3"
      * @see twitter4j.StatusStream
      * @see <a href="https://dev.twitter.com/docs/streaming-api/methods">Streaming API Methods statuses/firehose</a>
      * @since Twitter4J 2.0.4
      */
-    void geohose(final int count);
+    void geohose(final int count, final String partitions);
 
     /**
      * Returns a status stream of all public statuses. Available only to approved parties and requires a signed agreement to access. Please do not contact us about access to the firehose. If your service warrants access to it, we'll contact you.
